bc9c4c4ec2b63e91e999efeeceb6da5d39f9e517
dmitrij-pularija <dmitrij.pularija@gmail.com>
2022-10-22 22:35:15 +0300

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGallery() {
+  const items = [];
+  for (const galleryItem of galleryItems) {
+    const item = document.createElement("div");
+    const itemLink = document.createElement("a");
+    const itemImg = document.createElement("img");
+    item.classList.add("gallery__item");
+    itemLink.classList.add("gallery__link");
+    itemLink.setAttribute("href", galleryItem.original);
+    itemImg.classList.add("gallery__image");
+    itemImg.setAttribute("src", galleryItem.preview);
+    itemImg.setAttribute("alt", galleryItem.description);
+    itemImg.setAttribute("src-full", galleryItem.original);
+    itemLink.append(itemImg);
+    item.append(itemLink);
+    items.push(item);
+  }
+  gallery.append(...items);
+}
+
+createGallery();
+
+gallery.addEventListener("click", viewImg);
+function viewImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.getAttribute(
+      "src-full"
+    )}" width="800" height="600"/>`,
+    {
+      closable: false,
+      onShow: (instance) => {
+        document.addEventListener("keydown", esc.bind(instance));
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", esc.bind(instance));
+      },
+    }
+  );
+
+  instance.show();
+}
+
+function esc(event) {
+  if (!this.visible()) {
+    return;
+  }
+  if (event.code === "Escape") {
+    event.preventDefault();
+    this.close();
+  } else {
+    alert("Press Escape to return :}");
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery");
+const img1 = document.querySelector(".gimg");
 
-console.log(galleryItems);
+function createGallery() {
+  const items = [];
+  for (const galleryItem of galleryItems) {
+    const item = document.createElement("li");
+    const itemLink = document.createElement("a");
+    const itemImg = document.createElement("img");
+    item.classList.add("gallery__item");
+    itemLink.classList.add("gallery__link");
+    itemImg.classList.add("gallery__image");
+    itemLink.setAttribute("href", galleryItem.original);
+    itemImg.setAttribute("src", galleryItem.preview);
+    itemImg.setAttribute("alt", galleryItem.description);
+    itemLink.append(itemImg);
+    item.append(itemLink);
+    items.push(item);
+  }
+  gallery.append(...items);
+}
+
+createGallery();
+gallery.addEventListener("click", viewImg);
+
+function viewImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  var lightbox = new SimpleLightbox(".gallery__link", {
+    enableKeyboard: true,
+    docClose: false,
+    captions: true,
+    captionSelector: "img",
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+    close: false
+  });
+  lightbox.on("show.simplelightbox", () => {
+    document.addEventListener("keydown", esc);
+  });
+}
+function esc(event) {
+  if (event.code === "Escape") {
+    event.preventDefault();
+    document.removeEventListener("keydown", esc);
+  }
+}
